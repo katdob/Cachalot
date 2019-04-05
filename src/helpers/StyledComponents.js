7a8e9aed224885ae30eafee110bc6a334a9dd0fb
katdob <katdobbins@gmail.com>
2019-04-05 17:55:30 -0400

    added JSX for home screen, styled components, image asset

---
+++ src/helpers/StyledComponents.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+
+
+const HomeDivMain = styled.div``
+
+const HomeSectionTop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  margin: 1rem;`
+
+const HomeSectionMiddle = styled.div`
+  text-align: center;
+  position: relative;
+  top: 2rem;
+  left: 0;
+  margin: 0 0 0 0;`
+
+const Footer = styled.div`
+  margin: 2rem;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  text-align: center;`
+
+const Button = styled.button``
+
+const FooterButton = styled(Button)`
+  text-align: center;
+  border-radius: 0;
+  margin: 0 3rem 0 0;`
+
+const HomeJoinButton = styled(Button)`
+  text-align: center;
+  float: right;
+  border-radius: 0;
+  margin: 0 3rem 0 0;`
+
+const HeroText = styled.h3`
+  text-align: center;`
+
+export {
+  HomeDivMain,
+  HomeSectionTop,
+  HomeSectionMiddle,
+  Footer,
+  HomeJoinButton,
+  FooterButton,
+  HeroText
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
+import { HomeSectionTop, HomeDivMain, HomeJoinButton, HomeSectionMiddle,
+  Footer, FooterButton, HeroText } from '../helpers/StyledComponents'
+
 
 export default class Home extends Component {
-  //
+
   render() {
     return (
-      <div>
-        <header>
-          header
-        </header>
-        <body>
-          <p> body </p>
-        </body>
-      </div>
+      <HomeDivMain>
+        <HomeSectionTop>
+          <p>
+            The <b>Cachalot</b> Project
+            <HomeJoinButton>Join Us</HomeJoinButton>
+          </p>
+        </HomeSectionTop>
+        <HomeSectionMiddle>
+          <HeroText> Be  <b>Environment</b>  Aware </HeroText>
+          <h3> Find animal migrations near you. </h3>
+        </HomeSectionMiddle>
+        <Footer>
+          <FooterButton> Blog </FooterButton>
+          <FooterButton> Policies </FooterButton>
+          <FooterButton> Contact </FooterButton>
+        </Footer>
+      </HomeDivMain>
     )
   }
 }
