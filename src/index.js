900a186f52cbde7c01eefc046484b0e54b3d9089
katdob <katdobbins@gmail.com>
2019-04-05 15:23:44 -0400

    added screens Home, Contact, Blog, MyMigrations, Policies, helpers dir, components dir, assets dir, and StyledComponents helper

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Home from './screens/Home'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/screens/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+  //
+  render() {
+    return (
+      <div>
+        <header>
+          header
+        </header>
+        <body>
+          <p> body </p>
+        </body>
+      </div>
+    )
+  }
+}
