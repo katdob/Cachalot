a22ca228b7f3afeb7f27c094c05f994b90a20d0d
katdob <katdobbins@gmail.com>
2019-04-07 11:22:44 -0400

    closer background image placement, but now with unnecessary scrolling

--- src/helpers/HomeScreenStyledComponents.js
+++ src/helpers/HomeScreenStyledComponents.js
@@ -19,8 +19,9 @@ const HomeDivMain = styled.div`
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
-  height: 100%;
-  width: 100%;`
+  height: 65rem;
+  width: 100%;
+  overflow: hidden;`
 
 const HomeHeader = styled(Header)`
   color: white;`
